84a7deefca2ddc1d285faa50046d0c7063e89d12
Ajaygithubproject <145328880+Ajaygithubproject@users.noreply.github.com>
2026-05-02 18:58:37 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+
+  basePath: '/kicko-tech-test',
+  assetPrefix: '/kicko-tech-test/',
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
